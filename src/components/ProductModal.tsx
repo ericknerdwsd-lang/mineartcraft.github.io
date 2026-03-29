9dbc9773b5d2008da7e1dfa36853041d733f5795
Erick Fontenele <ericknerdwsd@gmail.com>
2026-03-29 10:44:58 -0300

    Revert Instagram link to profile URL version

--- src/components/ProductModal.tsx
+++ src/components/ProductModal.tsx
@@ -44,7 +44,7 @@ export default function ProductModal({
     setCurrentImageIndex((prev) => (prev - 1 + images.length) % images.length);
   };
 
-  const instagramLink = `https://www.instagram.com/direct/t/${instagramUsername}/`;
+  const instagramLink = `https://www.instagram.com/${instagramUsername}/`;
 
   return (
     <div className={styles.overlay} onClick={onClose}>
